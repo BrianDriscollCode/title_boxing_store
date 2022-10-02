a703ec429c426ae20057e9d31e5a58a363bfd8f5
briandriscollcode <bdriscoll407@gmail.com>
2022-10-01 18:10:30 -0700

    fixed banners

--- client/src/reducers/Banner_Reducer.js
+++ client/src/reducers/Banner_Reducer.js
@@ -9,7 +9,7 @@ const banner_list = [
     {
         type: "boxing_glove",
         title: "Boxing Gloves",
-        image_link: "images/banners/Banner-Gloves_1.jpg",
+        image_link: "/images/banners/Banner-Gloves_1.jpg",
         description: "From design to performance, color to construction or fit to feel, TITLE Boxing has the largest selection of boxing gloves in the world. We feature the most popular and in-demand brands including the entire range of TITLE Boxing, Fighting, Pro Mex, Rival, Adidas, Golden Boy Boxing, Muhammad Ali, WBC and so much more. Our range of gloves runs from professional world title fight gloves to the largest selection of bag gloves, training gloves, sparring gloves, competition gloves, MMA gloves, fitness gloves, women’s gloves and youth gloves."
 
     },
@@ -17,7 +17,7 @@ const banner_list = [
 
         type: "shoes",
         title: "Boxing Shoes",
-        image_link: "images/banners/Banner-Apparel-Footwear_copy.jpg",
+        image_link: "/images/banners/Banner-Apparel-Footwear_copy.jpg",
         description: "Whether you need training shoes for the gym or technologically advanced boxing shoes for amateur and professional competition, TITLE Boxing has you covered with brands featuring TITLE, Fighting, Nike and more. Footwork, balance and speed are vital to all boxing demands during training, sparring and in the ring. Make sure you are prepared for battle with the greatest selection of quality."
 
     },
@@ -25,7 +25,7 @@ const banner_list = [
 
         type: "headgear",
         title: "Head Gear",
-        image_link: "images/banners/Banner-Headgear_1.jpg",
+        image_link: "/images/banners/Banner-Headgear_1.jpg",
         description: "It’s vital in boxing and combat sports to protect yourself so that you can train and perform at your peak and stay injury-free. We feature the greatest collection of training and competition headgear available anywhere. Select from traditional, full-face and no-contact training headgear, and USA Boxing approved amateur competition headgear from brands including the full line of TITLE Boxing, Fighting, Pro Mex, Rival, Adidas, Golden Boy Boxing, WBC and more."
 
     },
@@ -33,7 +33,7 @@ const banner_list = [
 
         type: "punching_bag",
         title: "Punching Bags",
-        image_link: "images/banners/Punching_Bags_1.jpg",
+        image_link: "/images/banners/Punching_Bags_1.jpg",
         description: "Punching bags are the oldest form of boxing and combat sport training dating back more than a century. And still today, punching bags provide the most advanced and beneficial training available. TITLE Boxing features the best selection of punching bags from traditional heavy bags, full-length Thai heavy bags, uppercut heavy bags, water filled heavy bags, wall mount heavy bags, freestanding heavy bags, heavy bag stands and hangers, speed bags, speed bag platforms, double end bags, and every accessory available to complete your punching bag demands. Brands featuring TITLE Boxing, Fighting, Pro Mex, Rival, Adidas, Century and more."
 
     }
